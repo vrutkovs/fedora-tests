ee9999467aedd35ffc4e570c8907f409d3d0b83b
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-21 16:57:58 +0200

    Improved scenario tags detection

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,7 @@ import sys
 
 
 def before_scenario(context, scenario):
-    if scenario.tag == 'requires_smoketest':
+    if u'requires_smoketest' in scenario.effective_tags:
         if not os.path.isfile('/smoketest_passed'):
             sys.stdout.write("Skipping %s as smoketest failed\n" % scenario.name)
             scenario.skip()
