a93d871fc37c19f127197995a18b28a0b933a35b
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-21 15:57:04 +0200

    Sepate debug output

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-echo "Updating the system"
+echo ">> Updating the system"
 dnf clean expire-cache
 dnf update -y
 
-echo "Enabling passwordless sudo"
+echo ">> Enabling passwordless sudo"
 sed -i s/#\ %wheel/%wheel/g /etc/sudoers
 
-echo "Adding a new test user"
+echo ">> Adding a new test user"
 useradd test
 echo "redhat" | passwd test --stdin
 usermod -aG wheel test
 
-echo "Installing test requirements"
+echo ">> Installing test requirements"
 dnf install -y python-behave systemd-python
 
-echo "Enable abrt autoreporting"
+echo ">> Enable abrt autoreporting"
 dnf install -y abrt abrt-addon*
 abrt-auto-reporting enabled
 systemctl start abrtd
 
-echo "Running behave tests"
+echo ">> Running behave tests"
 sudo -u test behave -f html -o /tmp/report.html -f plain; rc =$?
 rhts-submit-log -l /tmp/report.html
 
