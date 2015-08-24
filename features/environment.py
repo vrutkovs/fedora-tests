da820609be5b738e27471379e38ab482ff745ac9
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-25 00:31:29 +0200

    Store journal and abrt list diff for scenario

--- features/environment.py
+++ features/environment.py
@@ -2,13 +2,32 @@ import os.path
 import sys
 import subprocess
 from time import sleep
+from datetime import datetime
 
 
 def before_scenario(context, scenario):
+    # Skip scenarios which require smoketest if it hasn't started yet
     if u'requires_smoketest' in scenario.effective_tags:
         if not os.path.isfile('/smoketest_passed'):
             sys.stdout.write("Skipping %s as smoketest failed\n" % scenario.name)
             scenario.skip()
+
+    context.abrt_cli = subprocess.check_output("abrt-cli ls", shell=True)
+    context.start_time = datetime.now()
+
+
+def after_scenario(context, scenario):
+    # Save abrt diff here
+    new_abrt_cli = subprocess.check_output("sudo abrt-cli ls", shell=True)
+    diff = new_abrt_cli[len(context.abrt_cli):].decode('utf8')
+    if diff != '':
+        context.embed('text/plain', diff, caption="ABRT")
+
+    # Add journal entries to the journal
+    cmd = "sudo journalctl --no-pager -o short-monotonic -q -b --since=%s" %\
+        context.start_time.strftime("%H:%M:%S")
+    journal = subprocess.check_output(cmd, shell=True).decode('utf8')
+    context.embed('text/plain', journal, caption="journal")
 
 
 def after_tag(context, tag):
