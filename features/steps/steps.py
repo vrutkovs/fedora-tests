bf5a841df550a2326e178373adb9ac6dab9b4e59
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-24 21:52:28 +0200

    Logs: decode from unicode

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -81,4 +81,4 @@ def run_installed_test_for_package(context, prefix, package):
         raise e
     finally:
         if os.path.isfile(testout):
-            context.embed('text/plain', open(testout, 'r').read(), caption="Test output")
+            context.embed('text/plain', open(testout, 'r').read().encode('ascii', 'ignore'), caption="Test output")
