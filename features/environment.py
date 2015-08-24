6a7e1fd74a69ccfaa53df84437ef4e9276d3c3f4
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-24 16:38:04 +0200

    Fix stop_gdm tag

--- features/environment.py
+++ features/environment.py
@@ -14,18 +14,10 @@ def before_scenario(context, scenario):
 def after_tag(context, tag):
     if tag == 'stop_gdm':
         # Stop gdm and make sure its dead after scenario
-        cmd = "pgrep gnome-session"
         for attempt in xrange(0, 30):
             try:
-                out = subprocess.check_output(cmd, shell=True)
-                if out != '':
-                    cmd = "killall gnome-session && sudo systemctl stop gdm"
-                    try:
-                        subprocess.check_output(cmd, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        print(e.output)
-                else:
-                    return
-            except subprocess.CalledProcessError as e:
-                print(e.output)
+                subprocess.check_output("pgrep gnome-session", shell=True)
+                subprocess.check_output("killall gnome-session; sudo systemctl stop gdm", shell=True)
+            except subprocess.CalledProcessError:
+                break
             sleep(5)
