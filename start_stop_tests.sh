7a71381208473c67d3074019aabe57b957bb95cd
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-26 18:04:50 +0200

    Add start_stop_tests.sh

---
+++ start_stop_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+systemctl stop gdm
+
+sudo -u test behave features/start_stop_tests.feature -f html -k -o /tmp/start_stop.html -f plain; rc=$?
+rhts-submit-log -l /tmp/start_stop.html
+
+journalctl -b --no-pager -o short-monotonic > /tmp/journal.log
+rhts-submit-log -l /tmp/journal.log
+
+abrt-cli ls > /tmp/abrt.log
+rhts-submit-log -l /tmp/abrt.log
+
+rpm -qa | sort > /tmp/packages.list
+rhts-submit-log -l /tmp/packages.list
+
+exit $rc
