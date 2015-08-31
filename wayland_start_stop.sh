d6f21454181207ad9de1e13fe42df9245a56e2ef
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-31 18:35:29 +0200

    Stop gdm to avoid hanging start-stop tests

--- wayland_start_stop.sh
+++ wayland_start_stop.sh
@@ -5,6 +5,8 @@ systemctl stop gdm
 sudo -u test behave features/wayland_start_stop_apps.feature -f html -k -o /tmp/wayland_start_stop.html -f plain; rc=$?
 rhts-submit-log -l /tmp/wayland_start_stop.html
 
+systemctl stop gdm
+
 journalctl -b --no-pager -o short-monotonic > /tmp/journal.log
 rhts-submit-log -l /tmp/journal.log
 
