d4cef4d9eab79144a407ba3dea3b4f8e3d63aa6f
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-08-31 18:35:29 +0200

    Stop gdm to avoid hanging start-stop tests

--- start_stop_tests.sh
+++ start_stop_tests.sh
@@ -5,6 +5,8 @@ systemctl stop gdm
 sudo -u test behave features/start_stop_apps.feature -f html -k -o /tmp/start_stop.html -f plain; rc=$?
 rhts-submit-log -l /tmp/start_stop.html
 
+systemctl stop gdm
+
 journalctl -b --no-pager -o short-monotonic > /tmp/journal.log
 rhts-submit-log -l /tmp/journal.log
 
